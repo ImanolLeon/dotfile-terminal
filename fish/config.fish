ac4cd23d8a30df4575004cefa6f3b35fd533cc02
Imanol León <148376646+ImanolLeon@users.noreply.github.com>
2026-08-05 16:45:50 -0500

    Clean up fish config by removing unused aliases
    Removed several aliases for convenience commands in fish config.

--- fish/config.fish
+++ fish/config.fish
@@ -3,22 +3,10 @@ if status is-interactive
 
 #Alias para poder ver iconos 
 alias ls='eza --icons'
-#z para evitar usar --> cd
-zoxide init fish | source
 
 #Ir a principal
 alias principal='cd /mnt/c/Users/imano'
-#Llamar a archivo con información acerca de migraciones
-alias migracionesPhp='/home/imano/Documentos/utils/MigrationsPhp.sh'
 
-#llamar a credenciales de proyecto nhl
-alias credenciales='cat /home/imano/Documentos/utils/credenciales.txt'
-
-#llamar a hostinger
-alias hostinger='cat /home/imano/Documentos/utils/hostinger.txt'
-
-#llamar a powerShell
-alias powershell='/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe'
 #prompt
 starship init fish | source
 
